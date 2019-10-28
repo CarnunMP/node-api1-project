7bc0d9a463dc2c2ca9adeea59a1529e02eb4ea73
CarnunMP <carnun@hotmail.co.uk>
2019-10-28 14:13:59 +0000

    DELETE /api/users/:id done; first three endpoints re-written

--- index.js
+++ index.js
@@ -11,28 +11,47 @@ server.use(cors());
 server.get("/api/users/:id", getUserById);
 server.get("/api/users", getAllUsers);
 server.post("/api/users", createNewUser);
+server.delete("/api/users/:id", deleteUserById);
 server.get("*", handleDefaultReq);
 
 function getUserById(req, res) {
     db.findById(req.params.id)
         .then(data => {
             console.log(data);
-            res.json(data);
+            if (data) {
+                res.status(200).json({
+                    success: true,
+                    data
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Cannot find specified user.",
+                })
+            }
         })
         .catch(err => {
             console.log(err);
-        })
+            res.status(500).json({
+                success: false,
+                err,
+            });
+        });
 }
 
 function getAllUsers(req, res) {
     db.find()
         .then(data => {
             console.log(data);
-            res.json(data);
+            res.status(200).json(data);
         })
         .catch(err => {
             console.log(err);
-        })
+            res.status(500).json({
+                success: false,
+                err,
+            });
+        });
 }
 
 function createNewUser(req, res) {
@@ -44,11 +63,40 @@ function createNewUser(req, res) {
     db.insert(newUser)
         .then(data => {
             console.log("id of new user: " + data.id);
-            res.json(data);
+            res.status(201).json({
+                success: true,
+                data,
+            });
         })
         .catch(err => {
             console.log(err);
+            res.status(500).json({
+                success: false,
+                err,
+            });
+        });
+}
+
+function deleteUserById(req, res) {
+    db.remove(req.params.id)
+        .then(deleted => {
+            console.log(deleted);
+            if (deleted) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "Cannot find specified user.",
+                });
+            }
         })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                success: false,
+                err,
+            });
+        });
 }
 
 function handleDefaultReq(req, res) {
