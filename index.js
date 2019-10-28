5c3f3d15319c8ffa13f78b245e88dc8a4b29a91c
CarnunMP <carnun@hotmail.co.uk>
2019-10-28 13:24:09 +0000

    initial; boilerplating done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+const express = require("express");
+const cors = require("cors");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get("/api/users", getAllUsers);
+server.get("*", handleDefaultReq);
+
+function getAllUsers(req, res) {
+    db.find()
+        .then(data => {
+            //
+        })
+        .catch(err => {
+            //
+        })
+}
+
+function handleDefaultReq(req, res) {
+    res.json("Hello world");
+}
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("listening on port " + (process.env.PORT || 3000));
+})
